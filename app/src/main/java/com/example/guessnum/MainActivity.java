215308e0b717913ee2d753a2f536f1c6db58b2f8
Salim654 <114499541+Salim654@users.noreply.github.com>
2023-01-04 13:10:27 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/guessnum/MainActivity.java
+++ app/src/main/java/com/example/guessnum/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.guessnum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.InputType;
+import android.view.InputDevice;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,19 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
     boolean checkstart=false;
     String historique="";
-    Button confirm,reset;
+    Button confirm,reset,show;
     TextView result,histo,counter;
     EditText num;
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new DatabaseHelper(this);
         confirm=(Button) findViewById(R.id.confirm);
         reset=(Button) findViewById(R.id.reset);
         result=(TextView) findViewById(R.id.result);
         histo=(TextView) findViewById(R.id.historique);
         counter=(TextView) findViewById(R.id.counter);
         num=(EditText) findViewById(R.id.num);
+        show=findViewById(R.id.showData);
         if (checkstart==false)
         {
             reset.setText("Start");
@@ -76,20 +86,67 @@ public class MainActivity extends AppCompatActivity {
                     int score=timeleft-trys;
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                     builder.setTitle("Score");
-                    if(score>0)
-                        {
+                    if (score>50){
                         builder.setMessage("U won with  :"+ score +" Score ");
-                         }
+                        final EditText name = new EditText(MainActivity.this);
+                        name.setInputType(InputType.TYPE_CLASS_TEXT);
+                        builder.setView(name);
+
+                        // Set up the buttons
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String scoreFinal=Integer.toString(score);
+                                String nameFinal = name.getText().toString();
+                                Boolean checkinsert= db.insertData(nameFinal,scoreFinal);
+                                if (checkinsert == true){
+                                    Toast.makeText(getApplicationContext(),"Score Added",Toast.LENGTH_LONG).show();
+                                }else {
+                                    Toast.makeText(getApplicationContext(),"Score Not Added",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                    }
                     else
-                        {
+                    {
                         builder.setMessage("U have Failed : 0 Score");
-                        };
+                    };
                     AlertDialog alert = builder.create();
                     alert.show();
+                    confirm.setVisibility(View.INVISIBLE);
+
                 }
                 historique = historique + "\r\n" + num.getText().toString();
                 histo.setText(historique);
             }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = db.showAllData();
+                if (res.getCount()==0){
+                    Toast.makeText(getApplicationContext(),"No score exists",Toast.LENGTH_LONG).show();
+                }
+                StringBuffer buffer= new StringBuffer();
+                while (res.moveToNext()){
+                    buffer.append("Name :"+res.getString(0)+"\n");
+                    buffer.append("Score :"+res.getString(1)+"\n");
+                }
+
+                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("All Scores ");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }
+
         });
 
     }
@@ -98,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
         trys = 0 ;
         timeleft = 0;
         Random numRandom = new Random();
-        randnum = numRandom.nextInt(1000);
+        randnum = numRandom.nextInt(100);
         checkstart=true;
         historique="";
         num.setText("0");
@@ -108,5 +165,7 @@ public class MainActivity extends AppCompatActivity {
         confirm.setVisibility(View.VISIBLE);
         result.setText("Guess Number!");
     }
+
+
 
 }
